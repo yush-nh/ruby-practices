ee5a53c582019990f5ce08ba65d5c32c10736d1c
yush-nh <yushin.nihei@luxiar.com>
2024-04-06 20:29:28 +0900

    remaked calendar program
    1度目のコミットではChatGPT等を使用していたが身にならないと思ったため、公式ドキュメントのみを用いて0から改めて作成

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -2,24 +2,26 @@
 require 'date'
 require 'optparse'
 
-def display_calendar(year, month)
+def display_calendar(month, year)
   first_day = Date.new(year, month, 1)
   last_day = Date.new(year, month, -1)
+  
   puts "      #{month}月 #{year}"
   puts "日 月 火 水 木 金 土"
   print "   " * first_day.wday
 
   (first_day..last_day).each do |date|
-    print "#{date.day.to_s.rjust(2)} "
-    puts if date.wday == 6
+    print "#{date.day} ".rjust(3)
+    puts if date.saturday?
   end
 
-  # 最後に%が出ないように改行
-  puts
+  # 最後に%が表示されるため改行を用いて対処
+  puts ""
 end
 
-# コマンドオプションの設定
+# オプションの設定
 options = {}
+
 opt = OptionParser.new
 opt.on("-y year", Integer) { |year| options[:year] = year }
 opt.on("-m month", Integer) { |month| options[:month] = month }
@@ -28,9 +30,4 @@ opt.parse!(ARGV)
 year = options[:year] || Date.today.year
 month = options[:month] || Date.today.month
 
-if month < 1 || month > 12
-  puts "月は1月から12月の間で入力してください"
-  exit
-end
-
-display_calendar(year, month)
+display_calendar(month, year)
