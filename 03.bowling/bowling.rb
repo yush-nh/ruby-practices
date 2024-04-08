44a5717ad96eb01dcf01aa22968f373ab0fa6506
yush-nh <yushin.nihei@luxiar.com>
2024-04-08 13:56:39 +0900

    formatted code

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 require 'rubygems'
 
-score = ARGV[0]
-scores = score.split(",")
+obtained_score = ARGV[0]
+scores = obtained_score.split(',')
 shots = []
 
 scores.each do |score|
@@ -18,35 +18,33 @@ frames = []
 shots.each_slice(2) do |shot|
   frames << shot
 end
-frames << [0,0] if frames.size == 10
+frames << [0, 0] if frames.size == 10
 
 point = 0
 frames.each_cons(2).with_index do |(current_frame, next_frame), i|
+  # 10投目の処理
   if i == 9
-    # 10投目の処理
-    if current_frame[0] == 10 && next_frame[0] == 10
-      point += 20 + frames.last.sum
-    elsif current_frame.sum == 10
-      point += 10 + next_frame.sum
-    else
-      point += current_frame.sum
-    end
+    point += if current_frame[0] == 10 && next_frame[0] == 10
+               20 + frames.last.sum
+             elsif current_frame.sum == 10
+               10 + next_frame.sum
+             else
+               current_frame.sum
+             end
     break
-  else
-    # ストライクの処理
-    if current_frame[0] == 10
-      if next_frame[0] == 10
-        point += (current_frame[0] + next_frame[0] + frames[i+2][0])
-      else
-        point += (current_frame[0] + next_frame.sum)
-      end
-    # スペアの処理
-    elsif current_frame.sum == 10
-      point += (current_frame.sum + next_frame[0])
-    else
-      point += current_frame.sum
-    end
-    end
+  end
+
+  point += if current_frame[0] == 10 # ストライクの処理
+             if next_frame[0] == 10
+               current_frame[0] + next_frame[0] + frames[i + 2][0]
+             else
+               current_frame[0] + next_frame.sum
+             end
+           elsif current_frame.sum == 10 # スペアの処理
+             current_frame.sum + next_frame[0]
+           else
+             current_frame.sum
+           end
 end
 
 puts point
