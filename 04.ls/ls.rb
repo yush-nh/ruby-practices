3822537ed39db597ffc7d4cbc2ce179324407eb5
yush-nh <yushin.nihei@luxiar.com>
2024-04-09 19:26:39 +0900

    created ls

---
+++ 04.ls/ls.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+files = []
+Dir.open('.').each_child do |f|
+  files << f
+end
+
+files.reject! { |f| f.start_with?('.') }
+files.sort!
+cols = 3
+rows = files.size / cols
+rows += 1 if files.size % cols != 0
+max_filename_len = files.max_by(&:length).length
+
+def format_filename(filename, max_filename_length)
+  filename.to_s + "#{' ' * (max_filename_length - filename.length)}  "
+end
+
+# lsの表示
+def display_ls(files, cols, rows, max_filename_len)
+  rows.times do |i|
+    tmp = 0
+    cols.times do |j|
+      next if files[i + j + tmp].nil?
+
+      print format_filename(files[i + j + tmp], max_filename_len)
+      tmp += rows - 1
+    end
+    print "\n"
+  end
+end
+
+display_ls(files, cols, rows, max_filename_len)
