e11bf7966846e987ea2e6abe3a6b8fcd298bab68
yush-nh <yushin.nihei@luxiar.com>
2024-04-15 21:56:40 +0900

    refactor code

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,33 +1,29 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-files = []
-Dir.open('.').each_child do |f|
-  files << f
-end
-
-files.reject! { |f| f.start_with?('.') }
-files.sort!
-cols = 3
-rows = files.size / cols
-rows += 1 if files.size % cols != 0
-max_filename_len = files.max_by(&:length).length
+files = Dir.entries('.').reject { |f| f.start_with?('.') }.sort
 
 def format_filename(filename, max_filename_length)
   filename.to_s + "#{' ' * (max_filename_length - filename.length)}  "
 end
 
-def display_ls(files, cols, rows, max_filename_len)
-  rows.times do |i|
-    tmp = 0
-    cols.times do |j|
-      next if files[i + j + tmp].nil?
+def sort_files(files, cols)
+  rows = files.size / cols
+  rows += 1 if files.size % cols != 0
+  sliced_files = files.each_slice(rows).to_a
+  max_array_size = sliced_files.max_by(&:size).size
 
-      print format_filename(files[i + j + tmp], max_filename_len)
-      tmp += rows - 1
-    end
-    print "\n"
-  end
+  sliced_files.map! { |arr| arr + [nil] * (max_array_size - arr.size) }.transpose
 end
 
-display_ls(files, cols, rows, max_filename_len)
+sorted_files = sort_files(files, 3)
+max_filename_len = files.max_by(&:length).length
+
+sorted_files.each do |rows|
+  rows.each do |row|
+    next if row.nil?
+
+    print format_filename(row, max_filename_len)
+  end
+  print "\n"
+end
