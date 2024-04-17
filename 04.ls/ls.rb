fd75ac4f1d5701abd6a0e99679b3be62419fccde
yush-nh <yushin.nihei@luxiar.com>
2024-04-17 09:24:10 +0900

    refactor code

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,17 +1,19 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'debug'
+
 files = Dir.glob('*').sort
 
 def format_filename(filename, max_filename_len)
-  "#{filename.ljust(max_filename_len)}  "
+  filename.ljust(max_filename_len + 2)
 end
 
 def sort_files(files, cols)
   rows = files.size.ceildiv(cols)
-  sliced_files = files.each_slice(rows).to_a
+  sliced_files = files.each_slice(rows)
 
-  sliced_files.map! { |arr| arr + [nil] * (rows - arr.size) }.transpose
+  sliced_files.map { |arr| arr + [nil] * (rows - arr.size) }.transpose
 end
 
 sorted_files = sort_files(files, 3)
