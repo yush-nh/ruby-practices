37c0faf194039a14dee902ec2324dfa666a0c6c5
yush-nh <yushin.nihei@luxiar.com>
2024-04-23 14:07:49 +0900

    add tests

--- 06.bowling_object/test_frame.rb
+++ 06.bowling_object/test_frame.rb
@@ -1,18 +1,16 @@
+# frozen_string_literal: true
+
 require 'minitest/autorun'
 require_relative 'frame'
 
 class FrameTest < Minitest::Test
-  def test_total
+  def test_score
     frame = Frame.new(1, 4)
-    assert_equal 5, frame.total
-    strike_frame = Frame.new('X')
-    assert_equal 10, strike_frame.total
-    last_frame = Frame.new(4, 6, 8)
-    assert_equal 18, last_frame.total
+    assert_equal 5, frame.score
   end
 
   def test_stike?
-    frame1 = Frame.new('X')
+    frame1 = Frame.new(10, 0)
     assert frame1.strike?
     frame2 = Frame.new(2, 7)
     refute frame2.strike?
@@ -21,7 +19,7 @@ class FrameTest < Minitest::Test
   def test_spare?
     frame1 = Frame.new(2, 8)
     assert frame1.spare?
-    frame2 = Frame.new('X')
+    frame2 = Frame.new(10, 0)
     refute frame2.spare?
     frame3 = Frame.new(1, 1)
     refute frame3.spare?
