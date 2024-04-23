363e5828bfc1e9d1e4a59dbee255735d7db619b1
yush-nh <yushin.nihei@luxiar.com>
2024-04-23 14:11:13 +0900

    removed unnecessary initialize argument

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -1,23 +1,24 @@
+# frozen_string_literal: true
+
 require_relative 'shot'
 
 class Frame
-  attr_reader :first_mark, :second_mark, :third_mark
+  attr_reader :first_shot, :second_shot
 
-  def initialize(first_mark, second_mark = nil, third_mark = nil)
-    @first_mark = Shot.new(first_mark)
-    @second_mark = Shot.new(second_mark)
-    @third_mark = Shot.new(third_mark)
+  def initialize(first_mark, second_mark = nil)
+    @first_shot = Shot.new(first_mark)
+    @second_shot = Shot.new(second_mark)
   end
 
   def strike?
-    true if @first_mark.score == 10
+    @first_shot.mark == 10
   end
 
   def spare?
-    true if !strike? && @first_mark.score + @second_mark.score == 10
+    !strike? && @first_shot.mark + @second_shot.mark == 10
   end
 
-  def total
-    total = @first_mark.score + @second_mark.score + @third_mark.score
+  def score
+    @first_shot.mark + @second_shot.mark
   end
 end
