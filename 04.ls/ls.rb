23f8c3042f8f74e7e95545e8aaced3c94b02b7a3
yush-nh <yushin.nihei@luxiar.com>
2024-04-23 17:03:16 +0900

    Add -a option

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-files = Dir.glob('*').sort
+require 'optparse'
 
 def format_filename(filename, max_filename_len)
   filename.ljust(max_filename_len + 2)
@@ -14,14 +14,27 @@ def sort_files(files, cols)
   sliced_files.map { |arr| arr + [nil] * (rows - arr.size) }.transpose
 end
 
-sorted_files = sort_files(files, 3)
-max_filename_len = files.max_by(&:length).length
+def ls(options)
+  files = Dir.entries('.').sort
+  files.reject! { |f| f.start_with?('.') } unless options[:all]
+  sorted_files = sort_files(files, 3)
+  max_filename_len = files.max_by(&:length).length
 
-sorted_files.each do |rows|
-  rows.each do |row|
-    next if row.nil?
+  sorted_files.each do |rows|
+    rows.each do |row|
+      next if row.nil?
 
-    print format_filename(row, max_filename_len)
+      print format_filename(row, max_filename_len)
+    end
+    print "\n"
   end
-  print "\n"
 end
+
+# options settings
+options = {}
+
+opt = OptionParser.new
+opt.on('-a', '--all') { |a| options[:all] = a }
+opt.parse!(ARGV)
+
+ls(options)
